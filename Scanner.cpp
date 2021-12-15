bba65a579bf96fccef8aa8c046354d2469690289
CORP\achoure <achoure@qualys.com>
2021-12-15 21:32:23 +0530

    version up

--- Scanner.cpp
+++ Scanner.cpp
@@ -1183,7 +1183,7 @@ int32_t __cdecl wmain(int32_t argc, wchar_t* argv[]) {
   }
 
   if (!cmdline_options.no_logo) {
-    printf("Qualys Log4j Vulnerability Scanner (CVE-2021-44228/CVE-2021-45046) 1.2.14\n");
+    printf("Qualys Log4j Vulnerability Scanner (CVE-2021-44228/CVE-2021-45046) 1.2.15\n");
     printf("https://www.qualys.com/\n\n");
   }
 
