f642449234bb75d651e32378361298be1d1c9baa
Rom Walton <rwalton@qualys.com>
2021-12-17 20:20:24 -0800

    Start refactoring code base in preparation for remediation

---
+++ stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <windows.h>
+#include <dbghelp.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdio.h>
+#include <direct.h>
+#include <fcntl.h>
+#include <io.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
