880df84f53c8836f1a24b68f3727fbd45380eeb9
Nico Agten <68544113+nagten@users.noreply.github.com>
2021-12-29 15:14:11 +0000

    Update Scanner.h
    Add function definitions to scan mount points

--- Scanner.h
+++ Scanner.h
@@ -6,4 +6,5 @@ int32_t ScanFile(bool console, bool verbose, std::wstring file);
 int32_t ScanDirectory(bool console, bool verbose, std::wstring directory);
 int32_t ScanLocalDrives(bool console, bool verbose);
 int32_t ScanNetworkDrives(bool console, bool verbose);
-
+int32_t ScanLocalDrivesInclMountpoints(bool console, bool verbose);
+int32_t EnumMountPoints(bool console, bool verbose, LPCWSTR szVolume);
